d67a6703b6c08d4ab6ae92875d83b30faf9f85a4
SunghoCha <tjdgh3725@gmail.com>
2024-06-26 14:04:45 +0900

    first commit

---
+++ src/main/java/com/sh/metablog_prac/MetaBlogPracApplication.java
@@ -0,0 +1,13 @@
+package com.sh.metablog_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaBlogPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaBlogPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/metablog_prac/MetaBlogPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.metablog_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaBlogPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
