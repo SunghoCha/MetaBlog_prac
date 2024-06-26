21ad6bd003c514c4b054a4d22b5444f64c3f3869
SunghoCha <tjdgh3725@gmail.com>
2024-06-27 05:29:16 +0900

    Hodolog - 부분완성 마무리 커밋

---
+++ src/test/java/com/sh/metablog_prac/service/PostServiceTest.java
@@ -0,0 +1,19 @@
+package com.sh.metablog_prac.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @Test
+    @DisplayName("글 작성")
+    void test1() {
+        //given
+
+        //when
+
+        //then
+    }
+}
